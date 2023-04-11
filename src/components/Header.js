f940098bcba9b47554288139d8698e6f1efcaf58
marocena26 <mariaelena.arocena@gmail.com>
2023-04-11 12:54:06 +0200

    add symbols header - create basic structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 //react libraries
 import { NavLink } from "react-router-dom";
 //components
+import folder from "../images/folder.svg";
+import notifications from "../images/notifications.svg";
+import logout from "../images/logout.svg";
 
 //styles
 
@@ -8,28 +11,41 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Sigaus</h1>
-      <nav className="nav container">
-        <ul>
-          <li className="nav__item">
+      <nav className="header__nav">
+        <ul className="header__nav--list">
+          <li className="header__nav--list--item">
             <NavLink> Inicio </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="header__nav--list--item">
             <NavLink> Declaraciones </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="header__nav--list--item">
             <NavLink> Instalaciones </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="header__nav--list--item">
             <NavLink> Incidencias/Alertas </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="header__nav--list--item">
             <NavLink> Financiación </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="header__nav--list--item">
             <NavLink> Informes </NavLink>
           </li>
         </ul>
       </nav>
+      <section className="header__section">
+        <div>
+          <img src={notifications} alt="" />
+          <h2>Incidencias</h2>
+        </div>
+        <div>
+          <img src={folder} alt="" />
+          <h2>Perfil</h2>
+        </div>
+        <div>
+          <img src={logout} alt="" />
+        </div>
+      </section>
     </header>
   );
 }
